bb856963fed3a07eab721efc105243cd713e2cd5
Gary Ma <gary.ma@entelect.co.za>
2024-06-02 14:59:20 +0200

    added nav, BrowserRoute, routes, and route

--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 1. check your node version: node -v    & npm -v
     v20.13.1 (node)
     10.8.0 (npm)
-2. npm init
+2. npm init (create backend node.js project)
 3. install some packages: 
     npm i express
     npm i mongoose
     npm i nodemon
-    
+4. npx create-react-app frontend
+5. npm start    (frontend)
+6. install routing: npm i react-router-dom
 
---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Nav from './Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Component</h1>} />
+          <Route path="/add" element={<h1>AddProduct Component</h1>} />
+          <Route path="/update" element={<h1>UpdateProduct Component</h1>} />
+          <Route path="/logout" element={<h1>Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
