9aacc220055812c8efc1cb583c5e90ca5a02a134
Gary Ma <gary.ma@entelect.co.za>
2024-06-02 21:04:25 +0200

    Added signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import Nav from './Nav';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './components/signup/SignUp';
 
 function App() {
   return (
@@ -13,8 +15,10 @@ function App() {
           <Route path="/update" element={<h1>UpdateProduct Component</h1>} />
           <Route path="/logout" element={<h1>Logout Component</h1>} />
           <Route path="/profile" element={<h1>Profile Component</h1>} />
-        </Routes>
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>        
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div className='footer'>
+            <h3> E-comm Dashboard @ Gary Ma &copy; 2024</h3>
+        </div>
+    );
+};
+
+export default Footer;
