16fe2cd441aa45051ca320e223ca0c1ed06fdfc9
Gary Ma <gary.ma@entelect.co.za>
2024-06-03 23:10:22 +0200

    added signup integration and resolved the cors issue

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+require('./db/config');
+const User = require('./db/User');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("app is working...");
-});
+// middleware to get json body from request
+app.use(express.json());
+app.use(cors({origin: true}));
 
-app.listen(3000);
+app.post('/register', async(req, res) => {
+    console.log('Received from client...');
+    console.log(req.body);
+    let user = new User(req.body);
+    let result = await user.save();
+    res.send(result);
+})
+
+app.listen(5000);
