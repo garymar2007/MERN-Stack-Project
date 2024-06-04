cd82d215f874e991eb419c2b5266b3558bca11d4
Gary Ma <gary.ma@entelect.co.za>
2024-06-04 23:09:40 +0200

    added private component to control the visibility based on lcoal storage

--- frontend/src/components/signup/SignUp.js
+++ frontend/src/components/signup/SignUp.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
@@ -6,6 +6,13 @@ const SignUp = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    useEffect(() => {
+        const auth = localStorage.getItem("user");
+        if (auth) {
+            navigate('/');
+        }
+
+    }, []);
     const collectData = async () =>{
         console.warn(name, email,password);
         let data = await fetch("http://localhost:5000/register", {
