36bf9b63f26eba1ffb76764832f0875a2c3f3511
Gary Ma <gary.ma@entelect.co.za>
2024-06-06 22:18:15 +0200

    Added signup and login endpoints; removed the password from response body.

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,24 @@ app.post('/register', async(req, res) => {
     console.log(req.body);
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password;
     res.send(result);
+})
+
+app.post('/login', async (req, res) => {
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select('-password');
+        if (user) {
+            res.send(user);
+        } else {
+            res.send({"result": "No user found"});
+        }
+    } else {
+        res.send({"result": "Incorrect Username or Password"});
+    }
+    
+    
 })
 
 app.listen(5000);
