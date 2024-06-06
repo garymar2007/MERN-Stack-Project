a71314084624e782239be69bbdc1211ed45733c5
Gary Ma <gary.ma@entelect.co.za>
2024-06-06 22:43:33 +0200

    added login frontend page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignUp from './components/signup/SignUp';
+import SignUp from './components/SignUp';
 import PrivateComponent from './components/PrivateComponent';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
             <Route path="/" element={<h1>Product Component</h1>} />
             <Route path="/add" element={<h1>AddProduct Component</h1>} />
             <Route path="/update" element={<h1>UpdateProduct Component</h1>} />
-            <Route path="/logout" element={<h1>Logout Component</h1>} />
+            <Route path="/logout" element={<h1>Logout Component</h1>} />            
             <Route path="/profile" element={<h1>Profile Component</h1>} />            
           </Route>
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>        
       </BrowserRouter>
       <Footer />
